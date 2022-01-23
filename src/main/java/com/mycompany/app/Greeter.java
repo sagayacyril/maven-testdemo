108f01fd698ef2af63ab957ced5b8187c0f58059
sagayacyril <59475566+sagayacyril@users.noreply.github.com>
2022-01-23 14:59:09 +0530

    Update Greeter.java

--- src/main/java/com/mycompany/app/Greeter.java
+++ src/main/java/com/mycompany/app/Greeter.java
@@ -2,6 +2,6 @@ package com.mycompany.app;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world welcome to Maven Builds!!!";
+    return "Hello world welcome to SCM Poll Maven Builds!!!";
   }
 }
